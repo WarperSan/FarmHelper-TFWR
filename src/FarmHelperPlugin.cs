d12e1b910fc5149d7e72a4a6f35707f94e6fd236
WarperSan <leumas.ecole@gmail.com>
2025-02-24 20:14:55 -0500

    Removed unused menu system

--- src/FarmHelperPlugin.cs
+++ src/FarmHelperPlugin.cs
@@ -1,9 +1,6 @@
 ﻿using BepInEx;
 using FarmHelper.API.Attributes;
-using FarmHelper.API.Interfaces;
-using FarmHelper.API.UI;
 using FarmHelper.Helpers;
-using FarmHelper.UI;
 using HarmonyLib;
 
 // ReSharper disable StringLiteralTypo
@@ -15,7 +12,7 @@ namespace FarmHelper;
 /// </summary>
 [BepInPlugin("org.warpersan.farmhelper", "Farm Helper", "1.0.0.0")]
 [FarmInfo("WarperSan", "https://github.com/WarperSan/FarmHelper-TFWR")]
-public class FarmHelperPlugin : BaseUnityPlugin, IToggleable
+public class FarmHelperPlugin : BaseUnityPlugin
 {
     /// <summary>
     /// Instance of this plugin
@@ -28,15 +25,10 @@ public class FarmHelperPlugin : BaseUnityPlugin, IToggleable
         
         var harmony = new Harmony("org.warpersan.farmhelper");
         harmony.PatchAll();
-    }
 
-    public void OnAdd()
-    {
         LocalizerHelper.Add(ErrorHelper.WRONG_ARGUMENT_COUNT_ERROR, "{0} takes {1} arguments.\n\nExpected:\n{2}\n\nReceived:\n{3}");
         LocalizerHelper.Add(ErrorHelper.WRONG_ARGUMENTS_ERROR, "{0} expected '{1}' as the #{2} argument.\n\nInstead, it got {3}.");
         
         ModHelperFunctions.LoadAll();
-        PluginMenu.Create<PluginListMenu>();
-        PluginMenu.Create<TitleMenu>().Open();
     }
 }
