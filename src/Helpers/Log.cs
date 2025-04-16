58e651e3a7178e01c57b8088d27d12b36685427a
WarperSan <leumas.ecole@gmail.com>
2025-04-16 11:43:54 -0400

    Changed `Log` - Now can take multiple data at once - Now prints "null" if the result is actually null

--- src/Helpers/Log.cs
+++ src/Helpers/Log.cs
@@ -1,4 +1,5 @@
-﻿using BepInEx.Logging;
+﻿using System.Text;
+using BepInEx.Logging;
 
 namespace FarmHelper.Helpers;
 
@@ -7,17 +8,37 @@ namespace FarmHelper.Helpers;
 /// </summary>
 internal static class Log
 {
-    private static void LogSelf(object data, LogLevel level) => FarmHelperPlugin.Instance?.Logger?.Log(level, data ?? "null");
+    private static void LogSelf(object?[] data, LogLevel level)
+    {
+        var logger = FarmHelperPlugin.Instance.Logger;
+        
+        if (logger == null)
+            return;
+
+        var message = new StringBuilder();
+
+        for (var i = 0; i < data.Length; i++)
+        {
+            var content = data[i] ?? "null";
+
+            message.Append(content);
+
+            if (i < data.Length - 1)
+                message.Append(" ");
+        }
+        
+        logger.Log(level, message.ToString());
+    }
 
     /// <inheritdoc cref="BepInEx.Logging.ManualLogSource.LogDebug"/>
-    public static void Debug(object data) => LogSelf(data, LogLevel.Debug);
+    public static void Debug(params object?[] data) => LogSelf(data, LogLevel.Debug);
     
     /// <inheritdoc cref="BepInEx.Logging.ManualLogSource.LogInfo"/>
-    public static void Info(object data) => LogSelf(data, LogLevel.Message);
+    public static void Info(params object?[] data) => LogSelf(data, LogLevel.Message);
     
     /// <inheritdoc cref="BepInEx.Logging.ManualLogSource.LogWarning"/>
-    public static void Warning(object data) => LogSelf(data, LogLevel.Warning);
+    public static void Warning(params object?[] data) => LogSelf(data, LogLevel.Warning);
     
     /// <inheritdoc cref="BepInEx.Logging.ManualLogSource.LogError"/>
-    public static void Error(object data) => LogSelf(data, LogLevel.Error);
+    public static void Error(params object?[] data) => LogSelf(data, LogLevel.Error);
 }
